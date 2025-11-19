1f2705c956484e20b4faa3f6426ce2e6d70f36dc
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2025-11-19 10:28:43 +0100

    feat(route) : suppression de la protection de la page home
    [DONE]

--- start/routes.ts
+++ start/routes.ts
@@ -11,5 +11,6 @@ import auth from '#middleware/auth_middleware'
 
 router.get('/login', '#controllers/auth_controller.login')
 router.post('/login', '#controllers/auth_controller.authenticate')
+router.get('/', ({ view }) => view.render('pages/login'))
 
-router.get('/home', ({ view }) => view.render('pages/home')).use(auth)
+router.get('/home', ({ view }) => view.render('pages/home'))
