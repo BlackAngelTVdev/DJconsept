cab9e8aad27fe2fa54d2dcd9ef7f216009895ec3
Albert Braimi <albertbraimi@gmail.com>
2025-11-26 11:20:21 +0100

    fix(docker-compose):

--- start.sh
+++ start.sh
@@ -2,6 +2,10 @@
 
 LOCK_FILE="init.lock"
 
+until nc -z -v -w30 "$DB_HOST" "$DB_PORT"; do
+  sleep 2
+done
+
 if [ ! -f "$LOCK_FILE" ]; then
   node ace generate:key
   node ace migration:fresh --seed
