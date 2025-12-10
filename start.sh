ca190c8918ce17d23ecc32ef31220bae5c9abb06
Albert Braimi <albertbraimi@gmail.com>
2025-12-10 11:06:33 +0100

    wait pour la db dans start.sh

--- start.sh
+++ start.sh
@@ -2,10 +2,12 @@
 
 LOCK_FILE="init.lock"
 
-until nc -z -v -w30 "$DB_HOST" "$DB_PORT"; do
-  echo "DB inaccessible"
-  sleep 2
-done
+echo "Waiting for database..."
+echo DB_NAME: ${DB_NAME}
+echo DB_HOST: ${DB_HOST}
+echo DB_PORT: ${DB_PORT}
+while ! nc -z ${DB_HOST} ${DB_PORT}; do sleep 1; done
+echo "Connected to database."
 
 if [ ! -f "$LOCK_FILE" ]; then
   node ace generate:key
