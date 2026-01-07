9cd1be75688a9c2f386603c3af3077768f2dad98
Albert Braimi <albertbraimi@gmail.com>
2026-01-07 16:33:46 +0100

    jai plus d'inspi

--- start.sh
+++ start.sh
@@ -9,10 +9,6 @@ echo DB_PORT: ${DB_PORT}
 while ! nc -z ${DB_HOST} ${DB_PORT}; do sleep 1; done
 echo "Connected to database."
 
-if [ ! -f "$LOCK_FILE" ]; then
-  node ace generate:key
-  node ace migration:fresh --seed
-  touch "$LOCK_FILE"
-fi
+node ace migration:run --force
 
 npm run dev
