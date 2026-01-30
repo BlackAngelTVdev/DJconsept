9ed1ce86a8e4a628fcc9265972e07f070560d776
BlackAngelTVdev <dami.scoot3@gmail.com>
2026-01-30 18:25:39 +0100

    Feat(home,work): la page home et le page de comment ca marche

--- start/routes.ts
+++ start/routes.ts
@@ -9,4 +9,7 @@
 
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+router.on('/').render('pages/home').as('home')
+router.on('/comment-ca-marche').render('pages/how_it_works').as('how-it-works')
+router.on('/login').render('pages/login').as('login')
+router.on('/register').render('pages/register').as('register')
