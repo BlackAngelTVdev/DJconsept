02c714127b5effda10a3a82d690654b4e4e6679c
Damien Rochat <156111574+BlackAngelTVdev@users.noreply.github.com>
2026-02-06 15:43:11 +0100

    Feat(icon): profil avec icon link
    [30][DONE]

--- database/seeders/user_seeder.ts
+++ database/seeders/user_seeder.ts
@@ -3,33 +3,58 @@ import User from '#models/user'
 
 export default class extends BaseSeeder {
   async run() {
-    // On utilise createMany pour aller plus vite
     await User.createMany([
       {
-        fullName: 'DJ Dam\'s',
+        fullName: "DJ Dam's",
         email: 'dami.scoo3@gmail.com',
         password: 'Tw4t2LuRq!',
+        location: 'Lausanne, CH',
+        pricePerGig: 600,
+        instagramUrl: 'https://www.instagram.com/dj_dams_official/',
+        tiktokUrl: 'https://www.tiktok.com/@dj_dams',
+        youtubeUrl: 'https://www.youtube.com/@djdams',
       },
       {
         fullName: 'Sarah Mendoza',
         email: 'sarah.m@electro-pulse.ch',
         password: 'Password123!',
+        location: 'Genève, CH',
+        pricePerGig: 450,
+        instagramUrl: 'https://www.instagram.com/sarah_m_official',
+        tiktokUrl: 'https://www.tiktok.com/@sarahmendoza',
       },
       {
         fullName: 'Marc Voisin',
         email: 'marc.voisin@booking-agency.fr',
         password: 'Password123!',
+        location: 'Paris, FR',
+        pricePerGig: 800,
+        youtubeUrl: 'https://www.youtube.com/@marcvoisin_live',
       },
       {
         fullName: 'Lucas ‘Dj Sky’ Dupont',
         email: 'sky.dj@gmail.com',
         password: 'Password123!',
+        location: 'Lyon, FR',
+        pricePerGig: 350,
+        instagramUrl: 'https://www.instagram.com/djsky_dupont',
+        tiktokUrl: 'https://www.tiktok.com/@djsky',
       },
       {
         fullName: 'Damien Rochat',
         email: 'admin@djconsept.fr',
         password: 'admin',
-        isAdmin: true
+        location: 'Morges',
+        isAdmin: true,
+        instagramUrl: 'https://www.instagram.com/dj_dams08',
+        youtubeUrl:'https://www.youtube.com/@Dj-Dam-s',
+      }
+      ,
+      {
+        fullName: 'GM',
+        email: 'gm@djconsept.fr',
+        password: 'gmguest',
+        location: 'Lausanne',
       }
     ])
   }
