101c80074edf5a6eca0bf7dcd25e6f1473c2a10f
BlackAngelTVdev <dami.scoot3@gmail.com>
2026-02-06 19:58:58 +0100

    Fix(profil) : presque tt la carde est clicable
    [20][DONE]
    encore des problèmes avec les autre lien

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,20 @@ export default defineConfig({
   plugins: [
     adonisjs({
       entrypoints: ['resources/css/app.css', 'resources/js/app.js'],
+      reload: ['resources/views/**/*.edge'],
     }),
   ],
+
+  // AJOUTE ÇA ICI : 
+  // Ça dit à Vite : "N'utilise aucun plugin PostCSS, reste en CSS pur"
+  css: {
+    postcss: {
+      plugins: []
+    }
+  },
+
+  server: {
+    allowedHosts: ['dj.laxacube.ch'],
+    host: '0.0.0.0', // Important si tu repasses sur Docker plus tard
+  },
 })
